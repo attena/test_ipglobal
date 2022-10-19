828a24e1f814b743a321fc4a3d017dd316ead250
Carolina Pulgarin <carolina.pulgarin@ab-inbev.com>
2022-10-19 17:48:50 +0200

    ajuste .env

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -1,3 +1,3 @@
-export const URL_API = process.env.REACT_APP_URL_API || 'https://api.themoviedb.org';
-export const API_KEY = process.env.REACT_APP_API_KEY || '8f781d70654b5a6f2fa69770d1d115a3';
-export const URL_IMG = process.env.REACT_APP_URL_IMG || 'https://image.tmdb.org/t/p/w500';
+export const URL_API = process.env.REACT_APP_URL_API || '';
+export const API_KEY = process.env.REACT_APP_API_KEY || '';
+export const URL_IMG = process.env.REACT_APP_URL_IMG || '';
